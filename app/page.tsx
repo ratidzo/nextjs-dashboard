ea216a6e7bc40cc1fab8c82fd4e25ead3abc1b5c
raty <ratygore@gmail.com>
2024-05-04 16:33:41 +0200

    create dashboard pages

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,14 @@ export default function Page() {
             width={1000}
             height={760}
             className="hidden md:block"
-            alt="Screenshots of the dashboard project showing the app displayed on desktop, tablet and mobile."
+            alt="Screenshots of the dashboard project showing the desktop version."
+          />
+          <Image
+            src={'/hero-mobile.png'}
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing the mobile version."
           />
         </div>
       </div>
