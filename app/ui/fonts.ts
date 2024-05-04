138dcad1f001b1a7d9d2f6ff12cfec2ce75c4ee5
raty <ratygore@gmail.com>
2024-05-04 12:28:03 +0200

    add lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,13 @@
-import { Poppins } from 'next/font/google';
+import { Poppins, Lusitana } from 'next/font/google';
+
 export const poppins = Poppins({
   weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
   style: 'normal',
   subsets: ['latin'],
 });
